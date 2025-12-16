9fb9b3079479832637022dc0822c50ece611d3ad
shakilahmedatik <shakilatik@gmail.com>
2025-12-17 00:26:26 +0600

    feat: Add initial database and Cloudinary configuration, updating environment variables and dependencies.

--- backend/config/env.js
+++ backend/config/env.js
@@ -2,6 +2,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const ENV = {
-    NODE_ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 3000,
+    DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/table-tales',
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    INGEST_SIGNING_KEY: process.env.INGEST_SIGNING_KEY,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import path from 'path'
 import { ENV } from '../config/env.js'
+import { connectDB } from '../config/db.js'
+import { clerkMiddleware } from '@clerk/express'
+
+
 const app = express()
+app.use(clerkMiddleware())
 
 const __dirname = path.resolve()
 
@@ -20,6 +25,7 @@ if(ENV.NODE_ENV==="production"){
 
 app.listen(ENV.PORT, () => {
     console.log(`Server is running on port ${PORT}`)
+    connectDB()
 })
 
 export default app
