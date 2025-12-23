efd4fa81c20e7b200e2d219eb9a5333670ff207c
shakilahmedatik <shakilatik@gmail.com>
2025-12-24 00:22:36 +0600

    fix: update LoginPage structure for proper rendering and adjust Vercel routing to direct to index.html

--- admin/src/pages/LoginPage.jsx
+++ admin/src/pages/LoginPage.jsx
@@ -2,9 +2,9 @@ import { SignIn } from '@clerk/clerk-react'
 
 const LoginPage = () => {
   return (
-    <>
+    <div className="flex justify-center items-center h-screen">
       <SignIn />
-    </>
+    </div>
   )
 }
 
