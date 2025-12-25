b3c8acba8b75aab6cc934fc1f3731ed51c481061
shakilahmedatik <shakilatik@gmail.com>
2025-12-25 20:19:10 +0600

    feat: Add Sentry for error tracking and session replay.

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import * as Sentry from '@sentry/react'
 import { BrowserRouter } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
@@ -14,6 +15,20 @@ if (!PUBLISHABLE_KEY) {
     'Missing VITE_CLERK_PUBLISHABLE_KEY. Add your Clerk Publishable Key to .env.local'
   )
 }
+console.log(import.meta.env.VITE_NODE_ENV)
+const dsn = import.meta.env.VITE_SENTRY_DSN
+if (!dsn) {
+  throw new Error('Missing VITE_SENTRY_DSN. Add your Sentry DSN to .env.local')
+}
+Sentry.init({
+  dsn,
+  sendDefaultPii: true,
+  integrations: [Sentry.replayIntegration()],
+  replaysSessionSampleRate:
+    import.meta.env.VITE_NODE_ENV === 'development' ? 1.0 : 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
+
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
